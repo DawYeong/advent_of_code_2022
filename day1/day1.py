c01bd5dd79557257ffe2475b8bc49db492b437e7
Benny Lam <bdylam@uwaterloo.ca>
2022-12-13 22:42:16 -0500

    Finish day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,25 @@
+from typing import List
+
 FILE_NAME = "input.txt" # relative path name
 
-def solve_part_1():
-    with open(FILE_NAME, 'r') as file:
+def solve_part_1(file_name: str) -> List[int]:
+    with open(file_name, 'r') as file:
         data = file.read()
 
     elf_total_calories = [sum(map(int, elf_calories.replace('\n', " ").split(" "))) for elf_calories in data.split('\n\n')]
-    max_elf_calories = max(elf_total_calories)
-    print(max_elf_calories)
+    # max_elf_calories = max(elf_total_calories)
+
+    return elf_total_calories
+
+def solve_part_2():
+    elf_total_calories = solve_part_1(FILE_NAME)
+    elf_total_calories.sort(reverse=True)
+
+    assert len(elf_total_calories) >= 3
+    top_3_calories = elf_total_calories[0] + elf_total_calories[1] + elf_total_calories[2]
+
+    print(top_3_calories)
+
 
 if __name__ == "__main__":
-    solve_part_1()
+    solve_part_2()
