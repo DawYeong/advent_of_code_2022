8af9b05d976685c091dff7b5c4b529101c781108
Benny Lam <bdylam@uwaterloo.ca>
2022-12-18 23:54:01 -0500

    Finish day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,6 +1,24 @@
+from dataclasses import dataclass
 from typing import List
+import os
 
+SCRIPT_FOLDER_PATH = os.path.dirname(os.path.realpath(__file__))
 FILE_NAME = "input.txt"
+
+
+# class naming is meh
+@dataclass(frozen=True)
+class TreeView:
+    is_visible: bool
+    num_of_visible_trees: int
+
+
+@dataclass(frozen=True)
+class TreeVisibility:
+    right: TreeView
+    left: TreeView
+    up: TreeView
+    down: TreeView
 
 
 def get_trees(file_name: str) -> List[List[int]]:
@@ -29,34 +47,53 @@ def get_up(trees: List[List[int]], row: int, col: int) -> List[int]:
 
 def get_down(trees: List[List[int]], row: int, col: int) -> List[int]:
     result = []
-    for i in range(row+1, len(trees[row])):
+    for i in range(row + 1, len(trees[row])):
         result.append(trees[i][col])
     return result
 
 
-def get_interior_trees(trees: List[List[int]]) -> int:
-    count = 0
+def get_tree_view(tree_height: int, other_trees: List[int]) -> TreeView:
+    num_of_trees = 0
+    for tree in other_trees:
+        num_of_trees += 1
+        if tree >= tree_height:
+            break
+
+    return TreeView(
+        is_visible=is_visible(
+            tree_height=tree_height,
+            other_trees=other_trees,
+        ),
+        num_of_visible_trees=num_of_trees,
+    )
+
+
+def get_tree_visibilities(trees: List[List[int]]) -> List[TreeVisibility]:
+    tree_visibilities = []
     for i in range(1, len(trees) - 1):
         for j in range(1, len(trees[0]) - 1):
             # determine left, right, up, down
+            left_list = trees[i][0:j]
+            left_list.reverse()
             tree_height = trees[i][j]
-            left = is_visible(
+
+            left = get_tree_view(
                 tree_height=tree_height,
-                other_trees=trees[i][0:j],
+                other_trees=left_list,
             )
-            right = is_visible(
+            right = get_tree_view(
                 tree_height=tree_height,
                 other_trees=trees[i][j + 1 : len(trees[i])],
             )
-            up = is_visible(
+            up = get_tree_view(
                 tree_height=tree_height,
                 other_trees=get_up(
-                    trees=trees,
-                    row=i,
-                    col=j,
-                ),
+                trees=trees,
+                row=i,
+                col=j,
+            ),
             )
-            down = is_visible(
+            down = get_tree_view(
                 tree_height=tree_height,
                 other_trees=get_down(
                     trees=trees,
@@ -64,22 +101,61 @@ def get_interior_trees(trees: List[List[int]]) -> int:
                     col=j,
                 ),
             )
+            tree_visibilities.append(
+                TreeVisibility(
+                    left=left,
+                    right=right,
+                    up=up,
+                    down=down,
+                )
+            )
+    return tree_visibilities
 
-            if left or right or up or down:
-                count += 1
 
-    return count
+def get_interior_trees(tree_visibilities: List[TreeVisibility]) -> int:
+    return len(
+        [
+            tree_visibility
+            for tree_visibility in tree_visibilities
+            if tree_visibility.left.is_visible
+            or tree_visibility.right.is_visible
+            or tree_visibility.up.is_visible
+            or tree_visibility.down.is_visible
+        ]
+    )
 
 
 def get_exterior_trees(trees: List[List[int]]) -> int:
     return 2 * (len(trees) + len(trees[0]) - 2)
 
 
+def get_score(tree_visibility: TreeVisibility) -> int:
+    return (
+        tree_visibility.left.num_of_visible_trees
+        * tree_visibility.right.num_of_visible_trees
+        * tree_visibility.up.num_of_visible_trees
+        * tree_visibility.down.num_of_visible_trees
+    )
+
+
+def get_max_score(tree_visibilities: List[TreeVisibility]) -> int:
+    scores = [get_score(tree_visibility) for tree_visibility in tree_visibilities]
+    
+    return max(scores)
+
+
 def solve_part_1(file_name: str):
     trees = get_trees(file_name)
-    visible_trees = get_interior_trees(trees) + get_exterior_trees(trees)
+    tree_visibilities = get_tree_visibilities(trees)
+    visible_trees = get_interior_trees(tree_visibilities) + get_exterior_trees(trees)
     print(visible_trees)
 
 
+def solve_part_2(file_name: str):
+    trees = get_trees(file_name)
+    tree_visibilities = get_tree_visibilities(trees)
+    max_score = get_max_score(tree_visibilities)
+    print(max_score)
+
 if __name__ == "__main__":
-    solve_part_1(FILE_NAME)
+    solve_part_2(f"{SCRIPT_FOLDER_PATH}/{FILE_NAME}")
