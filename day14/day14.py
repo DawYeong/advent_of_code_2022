bbf2e12e534f65ff8a945dfe51ec555770cde2e7
Benny Lam <bdylam@uwaterloo.ca>
2022-12-20 18:57:40 -0500

    Finish day 14 part 1

---
+++ day14/day14.py
@@ -0,0 +1,95 @@
+from dataclasses import dataclass
+import os
+from typing import Set
+
+SCRIPT_FOLDER_PATH = os.path.dirname(os.path.realpath(__file__))
+FILE_NAME = "input.txt"
+TEST_FILE_NAME = "test.txt"
+
+
+@dataclass(frozen=True)
+class Position:
+    x: int
+    y: int
+
+    def __eq__(self, other):
+        if isinstance(other, Position):
+            return self.x == other.x and self.y == other.y
+        return False
+
+    def __gt__(self, other):
+        return self.y > other.y
+
+
+def get_rocks(file_name: str) -> Set[Position]:
+    rocks = set()
+    with open(file_name, "r") as file:
+        while True:
+            data_line = file.readline()
+            if not data_line:
+                break
+
+            positions = [
+                list(map(int, pos.split(",")))
+                for pos in data_line.replace("\n", "").split(" -> ")
+            ]
+
+            # gets all pairs
+            for (x1, y1), (x2, y2) in zip(positions, positions[1:]):
+                x1, x2 = sorted([x1, x2])
+                y1, y2 = sorted([y1, y2])
+
+                # builds out the horizontal lines
+                for x in range(x1, x2 + 1):
+                    for y in range(y1, y2 + 1):
+                        rocks.add(Position(x=x, y=y))
+    return rocks
+
+
+def drop_sand(rocks: Set[Position]) -> bool:
+    curr_sand_pos = Position(x=500, y=0)
+    abyss = max(rocks).y + 1
+    while True:
+        check_pos = Position(x=curr_sand_pos.x, y=curr_sand_pos.y + 1)
+        if curr_sand_pos.y > abyss:
+            return False
+
+        if not check_pos in rocks:
+            # not blocked
+            curr_sand_pos = check_pos
+            continue
+
+        check_pos = Position(x=curr_sand_pos.x - 1, y=curr_sand_pos.y + 1)
+        if not check_pos in rocks:
+            curr_sand_pos = check_pos
+            continue
+
+        check_pos = Position(x=curr_sand_pos.x + 1, y=curr_sand_pos.y + 1)
+        if not check_pos in rocks:
+            curr_sand_pos = check_pos
+            continue
+
+        rocks.add(curr_sand_pos)
+        return True
+
+
+def simulate_sand(rocks: Set[Position]) -> int:
+    sand_added = 0
+    while True:
+        # check if anything is directly below
+        if drop_sand(rocks):
+            sand_added += 1
+        else:
+            break
+
+    return sand_added
+
+
+def solve_part_1(file_name: str):
+    rocks = get_rocks(file_name)
+    num_sand = simulate_sand(rocks)
+    print(num_sand)
+
+
+if __name__ == "__main__":
+    solve_part_1(f"{SCRIPT_FOLDER_PATH}/{FILE_NAME}")
