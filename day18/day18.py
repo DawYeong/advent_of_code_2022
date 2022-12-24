5e3a7c75a8612a77c789fd6b36cbc50d832bedf0
Benny Lam <bdylam@uwaterloo.ca>
2022-12-24 00:55:49 -0500

    Update it to consider sides instead of weird math

--- day18/day18.py
+++ day18/day18.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass
 import os
-from typing import Dict, List, Set, Tuple
+from typing import List, Set, Tuple
 
 
 SCRIPT_FOLDER_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -8,71 +7,38 @@ FILE_NAME = "input.txt"
 TEST_FILE_NAME = "test.txt"
 
 
-@dataclass
-class Cube:
-    x: int
-    y: int
-    z: int
-    sides: int = 6
+def get_faces(x: int, y: int, z: int) -> List[Tuple[int, int, int]]:
+    return [
+        (x + 1, y, z),
+        (x - 1, y, z),
+        (x, y + 1, z),
+        (x, y - 1, z),
+        (x, y, z + 1),
+        (x, y, z - 1),
+    ]
 
 
-def get_cubes(file_name: str) -> List[Cube]:
-    cubes = []
+def get_cubes(file_name: str) -> Set[Tuple[int, int, int]]:
+    cubes = set()
     with open(file_name, "r") as file:
         while True:
             data_line = file.readline()
             if not data_line:
                 break
             pos = data_line.strip().split(",")
-            cube = Cube(
-                x=int(pos[0]),
-                y=int(pos[1]),
-                z=int(pos[2]),
-            )
-
-            # check if cube touching other cubes
-            update_cubes(cubes=cubes, new_cube=cube)
-            cubes.append(cube)
+            cube = tuple(map(int, pos))
+            cubes.add(cube)
 
     return cubes
 
 
-def update_cubes(
-    cubes: List[Cube],
-    new_cube: Cube,
-):
-    for cube in cubes:
-        is_touching = (
-            (
-                cube.x == new_cube.x
-                and cube.y == new_cube.y
-                and abs(cube.z - new_cube.z) <= 1
-            )
-            or (
-                cube.x == new_cube.x
-                and cube.z == new_cube.z
-                and abs(cube.y - new_cube.y) <= 1
-            )
-            or (
-                cube.y == new_cube.y
-                and cube.z == new_cube.z
-                and abs(cube.x - new_cube.x) <= 1
-            )
-        )
-
-        if is_touching:
-            cube.sides -= 1
-            new_cube.sides -= 1
-
-
-def get_surface_area(cubes: List[Cube]) -> int:
-    return sum([cube.sides for cube in cubes])
-
-
 def solve_part_1(file_name: str):
     cubes = get_cubes(file_name)
-    surface_area = get_surface_area(cubes)
-    print(surface_area)
+    faces = []
+    for c in cubes:
+        faces.extend(get_faces(x=c[0], y=c[1], z=c[2]))
+    faces = [s for s in faces if s not in cubes]
+    print(len(faces))
 
 
 if __name__ == "__main__":
