c19f4add8784b5caca4cfc58f8f76f81473f164e
Benny Lam <bdylam@uwaterloo.ca>
2022-12-25 15:31:10 -0500

    Finish day 20 part 2

--- day20/day20.py
+++ day20/day20.py
@@ -7,17 +7,18 @@ FILE_NAME = "input.txt"
 TEST_FILE_NAME = "test.txt"
 
 
-def get_initial_state(file_name: str) -> List[int]:
+def get_initial_state(file_name: str, decryption_key: int = 1) -> List[int]:
     with open(file_name, "r") as file:
         data = file.read()
 
-    initial_state = [int(num) for num in data.split("\n")]
+    initial_state = [int(num) * decryption_key for num in data.split("\n")]
     return initial_state
 
 
-def encrypt(initial_state: List[int]) -> List[int]:
+def encrypt(initial_state: List[int], mix: int = 1) -> List[int]:
     indices = list(range(len(initial_state)))
-    for i in indices * 1:
+
+    for i in indices * mix:
         location = indices.index(i)
         # remove and insert in correct location
         indices.pop(location)
@@ -37,7 +38,7 @@ def determine_grove_sum(initial_state: List[int], encrypted_indices: List[int]) 
 
 
 def solve_part_1(file_name: str):
-    initial_state = get_initial_state(file_name)
+    initial_state = get_initial_state(file_name=file_name)
     encrypted_indices = encrypt(initial_state)
     grove_sum = determine_grove_sum(
         initial_state=initial_state, encrypted_indices=encrypted_indices
@@ -45,5 +46,21 @@ def solve_part_1(file_name: str):
     print(f"Grove sum: {grove_sum}")
 
 
+def solve_part_2(file_name: str):
+    decryption_key = 811589153
+    initial_state = get_initial_state(
+        file_name=file_name,
+        decryption_key=decryption_key,
+    )
+    encrypted_indices = encrypt(
+        initial_state=initial_state,
+        mix=10,
+    )
+    grove_sum = determine_grove_sum(
+        initial_state=initial_state, encrypted_indices=encrypted_indices
+    )
+    print(f"Grove sum: {grove_sum}")
+
+
 if __name__ == "__main__":
-    solve_part_1(f"{SCRIPT_FOLDER_PATH}/{FILE_NAME}")
+    solve_part_2(f"{SCRIPT_FOLDER_PATH}/{FILE_NAME}")
